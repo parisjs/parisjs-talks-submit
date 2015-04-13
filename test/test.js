c908ac8d4af4e02d15df99a95deda0bd8473fd80
Adrien Risser <risseraka@gmail.com>
2015-04-13 13:32:29 -0400

    Adding sponsoring form to /sponsoring

--- test/test.js
+++ test/test.js
@@ -16,6 +16,7 @@ app.set('host', 'http://localhost:3001');
 app.set('token', 'mytoken');
 app.set('key', 'mykey');
 app.set('idList', 'myIdList');
+app.set('idListSponsoring', 'mySponsoringIdList');
 
 describe('POST /', function() {
     it('should fail without good parameters', function(done) {
@@ -42,10 +43,12 @@ describe('POST /', function() {
         request(app)
             .post('/')
             .type('form')
-            .send({title: 'A new talk'})
-            .send({abstract: 'The abstract'})
-            .send({author: 'François francois@2metz.fr'})
-            .send({type: 'long'})
+            .send({
+                title: 'A new talk',
+                abstract: 'The abstract',
+                author: 'François francois@2metz.fr',
+                type: 'long'
+            })
             .expect(201)
             .end(function() {});
     });
@@ -62,10 +65,12 @@ describe('POST /', function() {
         request(app)
             .post('/')
             .type('form')
-            .send({title: 'A new talk'})
-            .send({abstract: 'The abstract'})
-            .send({author: 'François francois@2metz.fr'})
-            .send({type: 'short'})
+            .send({
+                title: 'A new talk',
+                abstract: 'The abstract',
+                author: 'François francois@2metz.fr',
+                type: 'short'
+            })
             .expect(201)
             .end(function() {});
     });
@@ -82,6 +87,60 @@ describe('POST /', function() {
             .post('/')
             .type('form')
             .send({title: 'A new talk'})
+            .end(function(err, res) {
+                assert.equal(res.status, 500);
+                done();
+            });
+    });
+});
+
+describe('POST /sponsoring', function() {
+    it('should fail without good parameters', function(done) {
+        request(app)
+            .post('/sponsoring')
+            .send()
+            .expect(400, done);
+    });
+
+    it('should post the result to the trello board', function(done) {
+        var trello = express();
+        trello.use(express.bodyParser());
+        var server = trello.listen(3001);
+        trello.post('/1/cards', function(req, res) {
+            assert.equal(req.query.key, 'mykey');
+            assert.equal(req.query.token, 'mytoken');
+            assert.equal(req.body.name, '2metz Inc. sponsoring');
+            assert.equal(req.body.desc, "never\n\nlieu + buffet\n\n**François francois@2metz.fr**");
+            assert.equal(req.body.idList, 'mySponsoringIdList');
+            assert.equal(req.body.labels, 'red');
+            server.close();
+            done();
+        });
+        request(app)
+            .post('/sponsoring')
+            .type('form')
+            .send({
+                entite: '2metz Inc.',
+                contact: 'François francois@2metz.fr',
+                period: 'never',
+                type: [ 'lieu', 'buffet' ]
+            })
+            .expect(201)
+            .end(function() {});
+    });
+
+    it('should fail when the trello API returns an error', function(done) {
+        var trello = express();
+        trello.use(express.bodyParser());
+        var server = trello.listen(3001);
+        trello.post('/1/cards', function(req, res) {
+            res.send(400);
+            server.close();
+        });
+        request(app)
+            .post('/sponsoring')
+            .type('form')
+            .send({contact: 'A new talk'})
             .end(function(err, res) {
                 assert.equal(res.status, 500);
                 done();
